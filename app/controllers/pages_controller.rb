cfb2a0c5734e74565320100a121887aa5107a1c6
Vasu Amara <vasuamara@vasus-air.attlocal.net>
2018-02-05 14:57:44 -0600

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+
+   def homepage
+     render text: "This is text coming from a rails app"
+   end
+
+ end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exampleapp::Application.config.secret_key_base = '07f476b1b0485ffdf973515eab13652f05fb9caec5b5c053d37899e36e75d85b2e93c107a54c6ee2c8670738c835d8cce8ecc554a6bba36a4bd77eeef165479c'
